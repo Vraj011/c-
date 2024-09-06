52fa2849a4215a92f1ffc066cc90e058930c9b0a
Vraj011 <vrajthakkar011@gmail.com>
2024-09-06 12:18:17 +0530

    first commit

---
+++ c programming/Basic Logic Programs/1_PRINTF.C
@@ -0,0 +1,9 @@
+// PROGRAM 1 : DISPLAY THE INFORMATION USING PRINTF - (START) -------------------->
+#include<stdio.h>
+void main(){
+    printf("Vraj Thakkar");
+    printf("30-1-2004");
+    printf("Age is 20");
+    printf("Isanpur, Ahmedabad");
+}
+// PROGRAM 1 : SIMPLE CALCULATOR - (END) <--------------------
